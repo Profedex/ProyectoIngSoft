f7d120969e3bce1cece39c2b3e45d8b56fe7c89b
Erick Fdo. Lopez Fimbres <y3rick_18@hotmail.com>
2015-05-01 16:59:15 -0700

    este archivo nos servira para validar la contraseña al momento de crear una cuenta nueva en el sistema

---
+++ src/validarContra.js
@@ -0,0 +1,44 @@
+<script type="text/javascript">
+
+		  function checkForm(form)
+		  {
+		    if(form.nombreU.value == "") {
+		      alert("Error: El usuario no puede estar en blanco!");
+		      form.nombreU.focus();
+		      return false;
+		    }
+		    re = /^\w+$/;
+		    if(!re.test(form.nombreU.value)) {
+		      alert("Error: El nombre de usuario solo puede contener numeros y letras!");
+		      form.nombreU.focus();
+		      return false;
+		    }
+
+		    if(form.pass.value != "") {
+		      if(form.pass.value.length < 6) {
+		        alert("Error: La contraseña debe tener al menos seis caracteres!");
+		        form.pass.focus();
+		        return false;
+		      }
+		      if(form.pass.value == form.nombreU.value) {
+		        alert("Error: La contraseña debe ser diferente al nombre de usuario!");
+		        form.pass.focus();
+		        return false;
+		      }
+		      if(form.pass.value != form.passC.value){
+		      	alert("Error: Las contraseñas no coinciden!");
+		        form.passC.focus();
+		        return false;
+		      }
+		      
+		    } else {
+		      alert("Error: Verifica que has introducido y confirmado tu contraseña");
+		      form.pass.focus();
+		      return false;
+		    }
+
+		    alert("You entered a valid password: " + form.pass.value);
+		    return true;
+		  }
+
+		</script>
