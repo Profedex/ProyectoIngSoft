5741707d09375c78ed0378ea37b7bb09e9765dc5
Erick Fdo. Lopez Fimbres <y3rick_18@hotmail.com>
2015-05-08 10:39:46 -0700

    se ha modificado el archivo

--- src/altaUsuario.php
+++ src/altaUsuario.php
@@ -1,19 +1,27 @@
 <?php
 	//conexion a la base de datos
 	if(isset($_POST['nombreU']) && !empty($_POST['nombreU']) &&
-		isset($_POST['correo']) && !empty($_POST['correo']) &&
-		isset($_POST['pass']) && !empty($_POST['pass'])){
+		isset($_POST['pass']) && !empty($_POST['pass']) &&
+		isset($_POST['passC']) && !empty($_POST['passC'])){
 
 		$conexion = mysql_connect('localhost','root','') or die ("No se pudo conectar al servidor");
 		mysql_select_db("profedex",$conexion) or die("No se pudo conectar a la base de datos");
 
 		$nombre = $_POST['nombreU'];
-		$email = $_POST['correo'];
 		$contra = $_POST['pass'];
-		mysql_query("INSERT INTO usuarios (nombreU,correo,passwUsuario) 
-			VALUES ('$nombre','$email','$contra')");
-		echo "Se han insertado los datos correctamente.";
-		header("Location: index.html");
+		$formulario = $_POST['form'];
+		
+
+		$UR = mysql_num_rows(mysql_query("SELECT nombreU FROM Usuarios where nombreU = '$nombre'"));
+		
+		if($UR != 0){
+			header("Location: crearCuenta.html");
+		}else{
+			mysql_query("INSERT INTO usuarios (nombreU,passwUsuario) 
+				VALUES ('$nombre','$contra')");
+			//header("Location: principal.html");
+			
+		}
 	}else{
 		
 		header("Location: crearCuenta.html");
