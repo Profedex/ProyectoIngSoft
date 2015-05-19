0dbad5adb7766bc83acbac19f4302919d4aac2dc
Erick Fdo. Lopez Fimbres <y3rick_18@hotmail.com>
2015-05-19 11:35:45 -0700

    Ahora ya identifica si es un usuario o un administrador dependiendo de como estes registrado en la base de datos, donde ambos tendran diferentes opciones, el administrador podra editar mientras que el usuario normal podra comentar

--- src/principal.php
+++ src/principal.php
@@ -1,6 +1,9 @@
 <?php
 include("conexion.php");
 session_start();
+if(!isset($_SESSION['usuarioLogiado'])){//si no se ha iniciado sesion te manara a la pagina para registrarse
+	header("location: registrarse.php");
+}
 ?>
 <html>
 <head>
@@ -20,16 +23,54 @@ session_start();
 		<nav>
 			<ul>
 				<li class="submenu">
-					<a href="#"><span class="icon-pacman"></span>Hola, <?php echo $_SESSION['usuarioLogiado'];?><span class="cara icon-like"></span></a>
+					<a href="#"><span class="icon-pacman"></span>Hola, <?php echo $_SESSION['usuarioLogiado'];?><span class="cara icon-unlocked"></span></a>
 					<ul class="hijos">
-						<li><a href="#">Submenu #1<span class="icon-coin-dollar"></span></a></li>
+						<li><a href="#">Inicio<span class="icon-home"></span></a></li>
 						<li><a href="#">Submenu #2<span class="icon-coin-dollar"></span></a></li>
-						<li><a href="cerrarSesion.php">Salir<span class="icon-coin-dollar"></span></a></li>
+						<li><a href="cerrarSesion.php">Salir<span class="icon-exit"></span></a></li>
 					</ul>
 				</li>
-				<li><a href="#"><span class="icon-users"></span>Acerca de Nososotros</a></li>
-				<li><a href="#"><span class="icon-book"></span>Materias</a></li>
-				
+				<?php 
+					if ($_SESSION['tipo'] == "admin") {
+					echo "<li><a href='usuariosRegistrados.php'><span class='icon-user-check'></span>Usuarios Registrados</a></li>";
+					echo "
+						<li class='submenu'>
+						<a href='#''><span class='icon-book'></span>Materias</a>
+							<ul class='hijos'>
+								<li><a href='verMaterias.php'>Ver Materias<span class='icon-eye'></span></a></li>
+								<li><a href='agregarMateria.php'>Agregar Materias<span class='icon-plus'></span></a></li>
+								<li><a href='editarMateria.php'>Editar Materias<span class='icon-settings'></span></a></li>
+								<li><a href='borrarMateria.php'>Borrar Materias<span class='icon-bin2'></span></a></li>
+							</ul>
+						</li>
+
+					";
+					echo "
+						<li class='submenu'>
+						<a href='#''><span class='icon-study'></span>Maestros</a>
+							<ul class='hijos'>
+								<li><a href='verMaestros.php'>Ver Maestros<span class='icon-eye'></span></a></li>
+								<li><a href='agregarMaestro.php'>Agregar Maestros<span class='icon-plus'></span></a></li>
+								<li><a href='editarMaestro.php'>Editar Maestros<span class='icon-settings'></span></a></li>
+								<li><a href='eliminarMaestro.php'>Borrar Maestros<span class='icon-bin2'></span></a></li>
+							</ul>
+						</li>
+
+					";
+					echo "
+						<li class='submenu'>
+						<a href='#''><span class='icon-bubbles2'></span>Comentarios Reportados</a>
+							<ul class='hijos'>
+								<li><a href='#'>Eliminar Comentarios<span class='icon-bin2'></span></a></li>
+							</ul>
+						</li>
+
+					";
+					}else{
+						echo "<li><a href='#'><span class='icon-users'></span>Acerca de Nososotros</a></li>";
+						echo "<li><a href='#'><span class='icon-book'></span>Materias</a></li>";
+					}
+				?>
 			</ul>
 		</nav>
 	</header>
